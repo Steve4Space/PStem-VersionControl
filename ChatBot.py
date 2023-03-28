3b7b08b13a08e3984485a80a6fc9437e400667a7
Stephen <102384025+Steve4Space@users.noreply.github.com>
2023-03-28 10:17:13 -0400

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -1,8 +1,10 @@
-import random
+import random, time
 
 def ask(say, ur, ur1, ur2):
+    time()
     print(say)
-    print(ur, "/n", ur1, ur2)
+    print(ur, "\n", ur1, "\n", ur2)
+    time()
     response = input("1, 2, or 3\n")
     return(response)
     
@@ -12,5 +14,12 @@ def main():
     responses = ["How're you?", "Why did you wane me up?! Fine, how're you, i'll be nice.. maybe", "What. Fiiiiiinnnnnnnneeeeeeeeee. How're you???"]
     humanr = ["Good!", "Bad:(", "Fine.", "Decent."]
     input = ask("Hello, " + responses[random], humanr[0], humanr[1], humanr[2])
+    
+def time(): #gotta prove python is slow
+    import time
+    from datetime import datetime
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
     
 main()
